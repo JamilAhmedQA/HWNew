f4427b582257997b2087654aa69dd5250d071982
JamilAhmedQA <jamilctrl@gmail.com>
2022-02-01 23:24:18 -0700

    02012022 11:24pm

--- src/hw7Q2UseOfNestedIf/DiabeticCondition.java
+++ src/hw7Q2UseOfNestedIf/DiabeticCondition.java
@@ -1,13 +1,12 @@
 package hw7Q2UseOfNestedIf;
-// Test 12
+
 import java.util.Scanner;
 
-public class DiabeticCondition { //windows ....
+public class DiabeticCondition {
 	public static void main(String[] args) {
 		System.out.println("<-- Please Enter your Hemoglobin A1C value below -->");
 		Scanner scanner = new Scanner(System.in);
 		double hbga1c = scanner.nextDouble();
-		
 		if (hbga1c > 6.4) {
 			System.out.println("I am a diabetic patient");
 		} else {
